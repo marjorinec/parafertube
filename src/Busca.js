fb48ebe4227c3f8ed9ead320ef56e421f3cf251c
Alline Marjorie <allinemarjorie@gmail.com>
2019-07-22 22:44:30 -0300

    Refatora o código de busca para seu próprio componente

---
+++ src/Busca.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { InputGroup, FormControl, Button, Row, Col, Form } from 'react-bootstrap'
+
+class Busca extends React.Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col>
+          <Form onSubmit={this.props.acao}>
+            <InputGroup className="search">
+              <FormControl
+                placeholder="Pesquisar"
+                value={this.state.termosBusca}
+                onChange={this.handleChange}
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" type="submit">Button</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </Col>
+    </Row>
+    )
+  }
+}
+
+export default Busca;
